38502f1701d3083323e77174818595d25dd3a299
(no author) <(no author)@fe66add1-021f-4af3-b75b-ea5154e73f91>
2011-04-01 18:38:51 +0000

    docs -> opengeo-docs
    git-svn-id: http://svn.opengeo.org/suite/trunk@2400 fe66add1-021f-4af3-b75b-ea5154e73f91

--- installer/linux/apt/build_opengeo-docs.sh
+++ installer/linux/apt/build_opengeo-docs.sh
@@ -9,7 +9,7 @@ DOCS=opengeosuite-$BRANCH-$REV-doc.zip
 get_file $BUILDS/$REPO_PATH/$DOCS yes
 
 # clean out old files
-rm -rf opengeo-docs/docs
+rm -rf opengeo-docs/opengeo-docs
 
 # unpack
 unzip files/$DOCS -d opengeo-docs
